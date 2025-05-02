4cc76cf034ad6e623f7e4417b68801901e7d045a
NickHemerycke <safrynick@gmail.com>
2025-05-02 15:05:06 +0200

    yeah fixing

--- nom_nomicon/app.py
+++ nom_nomicon/app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/dishes')
 def dishes():
-    return "<h1>Welcome to the Dishes Page</h1>"
+    return render_template("index.html")
 
 @app.route('/ingredients')
 def ingredients():
-    return "<h1>Welcome to the Ingredients Page</h1>"
+    return render_template("ingredients.html")
 
 @app.route('/menu')
 def menu():
-    return "<h1>Welcome to the Menu Page</h1>"
+    return render_template("menu.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
