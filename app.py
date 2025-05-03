5e14424d40e659216eeb5aa749e0c181f4ff698a
NickHemerycke <safrynick@gmail.com>
2025-05-03 16:37:29 +0200

    closer then ever

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-@app.route('/dishes')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+
+@app.route('/' , methods=['GET', 'POST'])
+@app.route('/dishes', methods=['GET', 'POST'])
 def home():
+    
     return render_template('index.html')
 
 @app.route('/ingredients')
